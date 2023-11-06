be627549a08f7baad928e7aca1af1d35f63d8e36
Arieu Sala <blognini.ariel.universidad@outlook.com>
2023-11-06 19:00:10 -0300

    Ok llegada a la base de Datos (resuelve por consola)

--- server/src/controllers/pingController.js
+++ server/src/controllers/pingController.js
@@ -1,3 +1,15 @@
+const connection = require('../models/conecction')
+
 module.exports.ping = (req, res) => {    
-    res.send('Este es un mensaje Pong de nuestro servidor PING');
-};
+    //res.send('Este es un mensaje Pong de nuestro servidor PING');
+    const consult = 'SELECT * FROM login';
+
+    try {
+        connection.query(consult, (err, results)=>{
+            console.log(results)
+        });
+
+    } catch (e) {
+
+    }
+}
