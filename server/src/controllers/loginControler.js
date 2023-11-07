f65811e8f1dbba10b572d87e492acd85eed8bfd9
Arieu Sala <blognini.ariel.universidad@outlook.com>
2023-11-07 00:20:33 -0300

    Config de LoginControler OK Validacion

--- server/src/controllers/loginControler.js
+++ server/src/controllers/loginControler.js
@@ -2,5 +2,30 @@ const connection = require('../models/conecction')
 
 module.exports.login = (req, res) => {
     const { username, password } = req.body;
-    console.log(username, password);
+    const consult = 'SELECT * FROM login WHERE username = ? AND password = ?';
+
+    try {
+        connection.query(consult, [username, password], (err, result) => {
+            if (err) {
+                res.send(err);
+            }
+
+            if (result.length > 0) {
+                res.send({message: 'Bienvenido'});
+                console.log(result);
+                res.json(result);
+            } else {
+                console.log('Usuario o contraseña incorrectos');
+                res.send({message: 'Usuario o contraseña incorrectos'});
+            }
+
+
+        });
+    } catch (e) {  
+
+    }
+
+
+
 }
+
